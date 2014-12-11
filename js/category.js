046dded00ff88bfa70f33a78c3a399ff71041d43
Boxuan ZHang <bz569@nyu.edu>
2014-12-10 21:47:45 -0500

    finish generating content for category tab dynamically

--- js/category.js
+++ js/category.js
@@ -3,6 +3,8 @@ Parse.initialize("z3WphiwOtJbwIi10PL65pLBLIt35aDQGj8kFO4MG", "UejR7LXTsAHgsYO5Q7
 var microsecondsPerWeek = 1000 * 60 * 60 * 24 * 14;
 var twoWeeksAgo = (new Date).getTime() - microsecondsPerWeek;
 var categories = new Array();
+var cateGoryFrequency = new Array();
+var top3Categoryies = new Array();
 
 //chrome.storage.local.set({'bilibili.com': "Streaming Media and Download"}, function() {
 //  // Notify that we saved.
@@ -33,7 +35,7 @@ chrome.history.search({
            //search for category info in local storage
            if (hostName != null){
                if (localStorage[hostName] != null) {                                        //find the category info locally
-                   document.write(hostName+"----"+localStorage[hostName]+"</br>");
+                   //document.write(hostName+"----"+localStorage[hostName]+"</br>");
                    insertCategoryInfoFor(hostName,localStorage[hostName]);
                }else {
                    //find the record in parse
@@ -67,16 +69,32 @@ chrome.history.search({
          }
        }
 
+        //sort the result
+        top3Categoryies = findTop3Category(cateGoryFrequency);
+
+        //set content for category tab
+        $("div#tab1 li#square1 a").attr("href","./SocialNetwork.html?topTab=category&contentTab=" + top3Categoryies[0]);
+        $("div#tab1 li#square1 a span").html(top3Categoryies[0])
+
+        $("div#tab1 li#square2 a").attr("href","./SocialNetwork.html?topTab=category&contentTab=" + top3Categoryies[1]);
+        $("div#tab1 li#square2 a span").html(top3Categoryies[1])
+
+        $("div#tab1 li#square3 a").attr("href","./SocialNetwork.html?topTab=category&contentTab=" + top3Categoryies[2]);
+        $("div#tab1 li#square3 a span").html(top3Categoryies[2])
      });
+
+
 
 function insertCategoryInfoFor(hostname, category){
   if (categories[category] == null){
-    categories[category] = [hostname];
+      categories[category] = [hostname];
+      cateGoryFrequency[category] = 1;
   }else{
-    var tmp = $.inArray(hostname, categories[category]);
+      cateGoryFrequency[category] += 1;
 
-    if(tmp == -1){
-      categories[category].push(hostname);
+      var tmp = $.inArray(hostname, categories[category]);
+      if(tmp == -1) {
+          categories[category].push(hostname);
     }
   }
 }
@@ -108,4 +126,37 @@ function askServerToUpateCategoryFor(hostName){
     xmlHttp.setRequestHeader("Content-Type",
         "application/x-www-form-urlencoded;");
     xmlHttp.send();
+}
+
+function findTop3Category(frequencies){
+    var result = new Array();
+
+    //find max
+    var max = 0
+    for(var key in frequencies){
+        if (frequencies[key] > max){
+            max = frequencies[key];
+            result[0] = key;
+        }
+    }
+
+    //find second
+    var second = 0
+    for(var key in frequencies){
+        if (frequencies[key] > second && key != result[0]){
+            second = frequencies[key];
+            result[1] = key;
+        }
+    }
+
+    //find third
+    var third = 0
+    for(var key in frequencies){
+        if (frequencies[key] > third && key != result[0] && key != result[1]){
+            third = frequencies[key];
+            result[2] = key;
+        }
+    }
+
+    return result;
 }
